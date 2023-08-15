20345aefbb9df5af4fcab19594549c531f126b21
tablonimus <lautamartin1995@gmail.com>
2023-08-14 21:58:26 -0300

    express

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # BoilerPlateSQL-ProtalentoB7
+
 Template SQL sequelize
+0 - crear un archivo index.js
+1 - `npm init -y` => creamos el package.json y lo editamos con la info que queriamos
+{
+"name": "boilerplatesql-protalentob7",
+"version": "1.0.0",
+"description": "Template SQL sequelize",
+"main": "index.js",
+"scripts": {
+"start": "node --watch index.js",
+"test": "echo \"Error: no test specified\" && exit 1"
+},
+"keywords": [],
+"author": "",
+"license": "ISC",
+
+}
+
+2- `npm install express --save` => se crea package-lock.json , la carpeta node_modules e instalamos express como dependencia
+
+3- Creamos un archivo llamado .gitignore, el cual ignora node_modules y package-lock.json en los push
+
+4- Agregar contenido al index (código levantar el servidor) y ejecutar el script `npm run start`
--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-const express = require('express')
-const server = express()
-const port = 3000 // en mi pc lo corro en el localhost:
+const express = require("express");
+const server = express(); //=> {}.get .post
+const port = 3000; // en mi pc lo corro en el localhost:
 
+//RUTAS Y ENDPOINTS //
+//http://localhost:3000
+//req => request => pedido
+//res => response => respuesta
+//server.get("/endpoint", (req,res)=>{ res.send("respuesta que quiero")})
 
-//RUTAS Y ENDPOINTS
-server.get('/ejemplo', (req, res) => {
-  res.send('Hello World!')
-})
+server.get("/product", (req, res) => {
+  /* QUERY -  peticion req.query */
+  // como armar un query =>luego del endpoint agregar ?clave=valor&clave=valor
+  let order = req.query.order;
+  if (order === "asc") {
+    console.log("envio resultados ordenados ascendentes");
+  }
+  res.send("Hello GET!");
+});
 
+server.get("/product/:producto", (req, res) => {
+  /* PARAMS - peticion de req.param */
+  //let params = req.params //es un objeto que guarda los parametros como valores, OJO que params es string
+  //con destructuring //INVESTIGAR :)
+  //let { producto } = req.params;
+  let producto = req.params.producto;
+  console.log("Esta ruta me traeria el producto: ", producto);
+  res.send("Coca-cola light");
+});
 
+server.post("/product", (req, res) => {
+  console.log("entrada a la ruta post"); //se va a la consola
+  res.send("Hello POST!"); //al body de nuestro cliente
+});
 
+server.patch("/product", (req, res) => {
+  console.log("entrada a la ruta patch"); //se va a la consola
+  res.send("Hello PATCH!"); //al body de nuestro cliente
+});
+
+server.delete("/product", (req, res) => {
+  console.log("entrada a la ruta delete"); //se va a la consola
+  res.send("Hello DELETE!"); //al body de nuestro cliente
+});
+
+/* Escucha del servidor */
 server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Esta corriendo el servidor correctamente en el puerto: ${port}`);
+});
